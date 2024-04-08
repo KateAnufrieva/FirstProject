0866574c0adcfc0c39cfd5090d0d744d797700ef
KateAnufrieva <93079863+KateAnufrieva@users.noreply.github.com>
2024-04-08 11:52:41 +0300

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,13 @@
-class Simple{
-    public static void main(String args[]){
-    System.out.println("Hello Java");
+import java.time.LocalTime;
+
+public class Simple {
+    public static void main(String[] args) {
+        String firstName = "Екатерина";
+        String lastName = "Ануфриева";
+        LocalTime currentTime = LocalTime.now();
+
+        System.out.println("Фамилия: " + lastName);
+        System.out.println("Имя: " + firstName);
+        System.out.println("Текущее время: " + currentTime);
     }
 }
